10b947dc84eac8ef02f9967ac76709d70868a3a4
Seung Hyun Kim <seung.kim0708@gmail.com>
2024-09-16 12:48:51 -0700

    added the initail middleware and routes for the backend server

---
+++ server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000; 
+const session = require('express-session');
+
+//Setting up Routes
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users');
+const productRoutes = require('./routes/products');
+const sellerRoutes = require('./routes/sellers');
+const cartRoutes = require('./routes/carts');
+const orderRoutes = require('./routes/orders');
+const paymentRoutes = require('./routes/payments')
+
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+app.use('/products', productRoutes);
+app.use('/seller', sellerRoutes);
+app.use('/cart', cartRoutes);
+app.use('/order', orderRoutes);
+app.use('/payment', paymentRoutes);
+
+
+//Configure session
+app.use(session({
+    secret: process.env.SECRET_KEY, 
+    ressave: false, 
+    saveUninitialized: false, 
+    cookie: {secure: true , maxAge: 1000 * 60 * 60 * 24}
+}))
+
+
+app.listen(PORT, () => console.log(`Server is running on http://locatlhost:${PORT}`))
